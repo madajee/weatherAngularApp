b2111d3ec096c1142224aac095d34ecc48c0c044
Jeetan Madaan <madajeeblog@gmail.com>
2019-02-05 09:55:29 -0500

    secondcommit

--- public/app.js
+++ public/app.js
@@ -15,14 +15,60 @@ weatherApp.config(function ($routeProvider) {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
     
 });
 
+//SERVICES
+weatherApp.service('cityService', function(){
+    this.city = "New York";
+});
+
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
-    
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams','cityService', function($scope, $resource, 
+    $routeParams, cityService) {
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || '2';  
+
+    $scope.weatherAPI = 
+        $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+            callback: "JSON_CALLBACK" }, {get: { method: "JSONP"}});
     
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: "appKey"});
+    console.log($scope.weatherResult);
+
+    $scope.convertToFahrenheit = function(degK){
+        return Math.round((1.8 * (degK-273)) + 32);
+    }
+
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
+
 }]);
+
+// DIRECTIVES
+weatherApp.directive('weatherReport', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        scope: {
+            weatherDay: "=",
+            convertToStandard: "&",
+            convertToDate: "&",
+            dateFormat: "@"
+        }
+    }
+});
